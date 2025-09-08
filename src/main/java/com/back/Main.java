44a5e461f361d1853a8d97d2cb10f3e3759d5fbd
junseokPP <pjs981130@gmail.com>
2025-09-08 20:20:44 +0900

    3단계 - 등록시 생성된 명언번호 노출

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String saying,author;
+        int count=0;
         System.out.println("== 명언 앱 ==");
         while(true){
             System.out.print("명령) ");
             String str = sc.nextLine();
-            String saying,author;
+
             if(str.equals("등록")){
                 System.out.print("명언 : ");
                 saying = sc.nextLine();
                 System.out.print("작가 : ");
                 author = sc.nextLine();
+                count++;
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(count));
             }
             if(str.equals("종료")){
                 break;
