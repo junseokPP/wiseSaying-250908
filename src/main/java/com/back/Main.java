ad29654648abaab293a80b6843fd1ea2eeed2297
junseokPP <pjs981130@gmail.com>
2025-09-08 20:08:32 +0900

    1단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -2,6 +2,8 @@ package com.back;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        System.out.print("종료");
     }
 }
