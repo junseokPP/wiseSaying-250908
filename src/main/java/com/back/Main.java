917223bbdf48f7d849d188e597cc00eb63d4afc9
junseokPP <pjs981130@gmail.com>
2025-09-08 20:19:14 +0900

    2단계 - 등록

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,9 +1,25 @@
 package com.back;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        System.out.print("종료");
+        while(true){
+            System.out.print("명령) ");
+            String str = sc.nextLine();
+            String saying,author;
+            if(str.equals("등록")){
+                System.out.print("명언 : ");
+                saying = sc.nextLine();
+                System.out.print("작가 : ");
+                author = sc.nextLine();
+            }
+            if(str.equals("종료")){
+                break;
+            }
+        }
     }
 }
