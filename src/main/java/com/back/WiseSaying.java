64a05bdfabd512cee8d6ed2074ce808d052293e0
junseokPP <pjs981130@gmail.com>
2025-09-08 20:36:03 +0900

    App 클래스 도입하여 Main분리

---
+++ src/main/java/com/back/WiseSaying.java
@@ -0,0 +1,14 @@
+package com.back;
+
+
+public class WiseSaying {
+    int id;
+    String saying;
+    String author;
+
+    public WiseSaying(int id, String saying, String author) {
+        this.id = id;
+        this.saying = saying;
+        this.author = author;
+    }
+}
