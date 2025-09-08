ea85dd0baac604b9eb8c90729dbb6c061d75c0e8
junseokPP <pjs981130@gmail.com>
2025-09-08 20:42:15 +0900

    actionWrite,actionList함수 도입하여 가독성 업

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -4,35 +4,47 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void run(){
-        Scanner sc = new Scanner(System.in);
-        String saying,author;
-        WiseSaying[] wiseSaying = new WiseSaying[100];
-        int count=0;
+    Scanner sc = new Scanner(System.in);
+    WiseSaying[] wiseSaying = new WiseSaying[100];
+    int count=0;
+
+    public  void run(){
+
+
         System.out.println("== 명언 앱 ==");
+
         while(true){
             System.out.print("명령) ");
             String str = sc.nextLine();
 
             if(str.equals("등록")){
-                System.out.print("명언 : ");
-                saying = sc.nextLine();
-                System.out.print("작가 : ");
-                author = sc.nextLine();
-                count++;
-                wiseSaying[count] = new WiseSaying(count,saying,author);
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(count)); //4단계인데,,이미 해버림
+                actionWrite();
             }
             else if(str.equals("목록")){
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-                for(int i=count;i>0;i--){
-                    System.out.println("%d / %s / %s".formatted(wiseSaying[i].id,wiseSaying[i].saying,wiseSaying[i].author));
-                }
+                actionList();
             }
             else if (str.equals("종료")){
                 break;
             }
         }
+    }
+
+    private void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for(int i=count;i>0;i--){
+            System.out.println("%d / %s / %s".formatted(wiseSaying[i].id,wiseSaying[i].saying,wiseSaying[i].author));
+        }
+    }
+
+    public void actionWrite() {
+
+        System.out.print("명언 : ");
+        String saying = sc.nextLine();
+        System.out.print("작가 : ");
+        String author = sc.nextLine();
+        count++;
+        wiseSaying[count] = new WiseSaying(count,saying,author);
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(count)); //4단계인데,,이미 해버림
     }
 }
