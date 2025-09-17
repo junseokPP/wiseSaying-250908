b79602e6096d20acbc17714c1ec5533f55cfebc0
junseokPP <pjs981130@gmail.com>
2025-09-17 16:40:14 +0900

    step2 : 27강 id로 찾는 for문을 스트림으로 WiseSaying객체 자체를 리턴하도록 변경

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -49,23 +49,22 @@ public class App {
         String idStr = commandBits[1];
         int id = Integer.parseInt(idStr);
 
-        int modifyTargetIndex = findIndexById(id);
+        WiseSaying wiseSaying  = findByIdOrNull(id);
 
-        if(modifyTargetIndex == -1) {
+        if(wiseSaying == null) {
             System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
             return;
         }
 
-        WiseSaying modifyTargetWiseSaying = wiseSayings.get(modifyTargetIndex);
 
-        System.out.println("명언(기존) : %s".formatted(modifyTargetWiseSaying.getSaying()));
+        System.out.println("명언(기존) : %s".formatted(wiseSaying.getSaying()));
         System.out.print("명언 : ");
         String newSaying = sc.nextLine();
-        System.out.println("작가(기존) : %s".formatted(modifyTargetWiseSaying.getAuthor()));
+        System.out.println("작가(기존) : %s".formatted(wiseSaying.getAuthor()));
         System.out.print("작가 : ");
         String newAuthor = sc.nextLine();
 
-        modify(modifyTargetWiseSaying, newSaying, newAuthor);
+        modify(wiseSaying, newSaying, newAuthor);
     }
 
     private void modify(WiseSaying modifyTargetWiseSaying, String newSaying, String newAuthor) {
@@ -94,8 +93,14 @@ public class App {
         }
     }
 
-    private int findIndexById(int id) {
+    private WiseSaying findByIdOrNull(int id) {
+        return wiseSayings.stream()
+                .filter(s -> s.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
 
+    private int findIndexById(int id) {
         return IntStream.range(0,wiseSayings.size())
                 .filter(i -> wiseSayings.get(i).getId() == id)
                 .findFirst()
@@ -104,16 +109,7 @@ public class App {
     }
 
     private boolean delete(int id) {
-
-        int deleteTargetIndex = findIndexById(id);
-
-        if (deleteTargetIndex == -1) {
-            return false;
-        }
-
-        wiseSayings.remove(deleteTargetIndex);
-
-        return true;
+        return wiseSayings.removeIf(wiseSaying -> wiseSaying.getId() == id);
     }
 
     private void actionList() {
